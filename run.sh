ec5aa3bcbf2cc14537f5a75c5482deb8c2c4cedd
pawc <pswid89@gmail.com>
2016-09-26 17:33:58 +0200

    initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+rm -rf bin
+mkdir bin
+javac -d bin src/main/java/pl/pawc/jokes/Main.java
+cd bin
+java pl.pawc.jokes.Main
---
+++ src/main/java/pl/pawc/jokes/Main.java
@@ -0,0 +1,9 @@
+package pl.pawc.jokes;
+
+class Main{
+	
+	public static void main(String args[]){
+		System.out.println("ok");
+	}
+
+}
